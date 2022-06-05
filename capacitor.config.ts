9b9f6fdeecefefe95a9a9c454ad49cbfe4e8b5f6
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2022-06-05 18:52:30 -0300

    Setup capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mhcofre-frontend-ionic',
+  appId: 'br.com.marcoshssilva.mhcofre',
+  appName: 'MHCofre',
   webDir: 'www',
   bundledWebRuntime: false
 };
