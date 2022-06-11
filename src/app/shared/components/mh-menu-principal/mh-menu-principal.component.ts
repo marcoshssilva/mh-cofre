eb073e624a730220f3a30c6b3f86e3def286572a
MARCOS HENRIQUE DE SANTANA <76234459+marcoshssilva@users.noreply.github.com>
2022-06-11 17:43:47 -0300

    feat implements menu-principal and toolbar on application

---
+++ src/app/shared/components/mh-menu-principal/mh-menu-principal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mh-menu-principal',
+  templateUrl: './mh-menu-principal.component.html',
+  styleUrls: ['./mh-menu-principal.component.scss'],
+})
+export class MhMenuPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IonicModule } from '@ionic/angular';
+import { MhToolbarPrincipalComponent } from './components/mh-toolbar-principal/mh-toolbar-principal.component';
+import { CoreModule } from '../core/core.module';
+import { MhMenuPrincipalComponent } from './components/mh-menu-principal/mh-menu-principal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MhToolbarPrincipalComponent,
+    MhMenuPrincipalComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    IonicModule,
+    CoreModule
+  ],
+  exports: [
+    CoreModule,
+    IonicModule,
+    MhToolbarPrincipalComponent,
+    MhMenuPrincipalComponent
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
