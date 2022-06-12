4346804f7945219d0f1ffeebe8d661676499a9ab
MARCOS HENRIQUE DE SANTANA <76234459+marcoshssilva@users.noreply.github.com>
2022-06-11 22:07:19 -0300

    feat recover message from translate by key from a service

--- src/app/core/services/mh-translate-helper.service.ts
+++ src/app/core/services/mh-translate-helper.service.ts
@@ -5,7 +5,6 @@ import { TranslateService } from '@ngx-translate/core';
   providedIn: 'root',
 })
 export class MhTranslateHelperService {
-
   public readonly defaultLanguage = 'pt-BR';
   constructor() {}
 
@@ -18,5 +17,9 @@ export class MhTranslateHelperService {
     translate.setDefaultLang(this.defaultLanguage);
 
     translate.use(this.defaultLanguage);
+  }
+
+  public getKey(key: string, translate: TranslateService, params?: object) {
+    return translate.get(key, {...params}).toPromise();
   }
 }
